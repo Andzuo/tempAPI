c724428520adecd9cebccfcb514c8d6d8d0a03bc
“neocrmoficial” <neocrmoficial@gmail.com>
2023-11-24 16:12:33 -0300

    doc: Adicionado responsividade ao layout

--- src/app/index.js
+++ src/app/index.js
@@ -5,15 +5,26 @@ function spotData(data) {
     document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°"
     document.querySelector(".weather").innerHTML = data.weather[0].description
     document.querySelector(".umity").innerHTML ="Umidade:" + data.main.humidity + "%"
+    document.querySelector(".temp-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     console.log(data);
 }
 
 async function searchcity (cidade) {
-    
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
+   try{ 
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    const responseData = await data.json();
 
+    if (responseData.cod && responseData.cod !== 200) {
+        throw new Error(`Erro na API: ${responseData.message}`);
+    }
+        spotData(responseData)
+    } catch (error) {
+        Spotalert()
+    }
+}
 
-    spotData(data)
+function Spotalert() {
+    alert("Insira uma cidade valida")
 }
 
 function searchButton() {
